aba10a0bc269156fcede8f2d4b7827c7ca3357e7
Cornelius Denninger <cornelius.denninger@gmail.com>
2019-04-08 15:38:31 +0200

    Update eventPage.js

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,4 @@
+var webhook = "https://discordapp.com/api/webhooks/564579809661943838/fNuj_dNry8ZfgC_5Q8iLItpdnUHUnn44pJIzVy90MT3NcSjwhA9JWZMz6ZtG9V_-m3TL";
 var contextMenuItem = {
 	"id": "discord",
 	"title": "discord_title",
@@ -10,6 +11,11 @@ chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(pageUrl) {
 	// var current_url = window.location.href;
+	var xhttp = new XMLHttpRequest();
+
 	var URL = pageUrl.pageUrl;
 	console.log(URL);
+	xhttp.open("POST", webhook, true);
+	xhttp.setRequestHeader("Content-Type", "application/json");
+	xhttp.send(JSON.stringify({"content":URL}));
 });
