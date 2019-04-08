ad5c2d5b92bc79ec3471d60acbb49f720ad9fe51
Cornelius Denninger <cornelius.denninger@gmail.com>
2019-04-08 15:45:25 +0200

    Added logo

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,7 @@
 var webhook = "https://discordapp.com/api/webhooks/564579809661943838/fNuj_dNry8ZfgC_5Q8iLItpdnUHUnn44pJIzVy90MT3NcSjwhA9JWZMz6ZtG9V_-m3TL";
 var contextMenuItem = {
 	"id": "discord",
-	"title": "discord_title",
+	"title": "Send Link to Discord",
 	"contexts": ["all"],
 };
 
