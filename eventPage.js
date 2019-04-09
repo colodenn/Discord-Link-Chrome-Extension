69e5c771196002ca9f17d86a2ac08d9aad7b3785
Cornelius Denninger <cornelius.denninger@gmail.com>
2019-04-09 20:56:13 +0200

    123

--- eventPage.js
+++ eventPage.js
@@ -6,25 +6,31 @@ var contextMenuItem = {
 };
 var webhook = "";
 chrome.contextMenus.create(contextMenuItem);
-chrome.storage.sync.get('webhook', function (data) {
-	console.log(data.webhook);
-	webhook = data.webhook;
-});
 chrome.contextMenus.onClicked.addListener(function (pageUrl) {
 	chrome.storage.sync.get('webhook', function (data) {
 		console.log(data.webhook);
 		webhook = data.webhook;
 	});
+	chrome.storage.sync.get('name1', function (data) {
+		console.log(data.name1);
+		name = data.name1;
+	});
+
+
 	var xhttp = new XMLHttpRequest();
 	var URL = pageUrl.pageUrl;
 	console.log(URL);
 	xhttp.open("POST", webhook, true);
 	xhttp.setRequestHeader("Content-Type", "application/json");
 	xhttp.send(JSON.stringify({
-		"content": URL
+		"content": "**"+ name + ":"  + "**   " + URL
 	}));
 });
 
 
-// chrome.storage.sync.set({ key : value}, function() {
-// });
+
+chrome.storage.sync.get('webhook', function (data) {
+	console.log(data.webhook);
+	webhook = data.webhook;
+	name = data.name;
+});
--- popout.js
+++ popout.js
@@ -1,5 +1,7 @@
 document.getElementById("webhook").value = localStorage.getItem("webhook");
+document.getElementById("name").value = localStorage.getItem("name");
 var webhookurl = localStorage.getItem("webhook");
+var name = localStorage.getItem("name");
 console.log("penis");
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,10 +17,13 @@ function savewebhook() {
     if (typeof (Storage) !== "undefined") {
         // Store
         var url = document.getElementById("webhook").value;
+        var name = document.getElementById("name").value;
         localStorage.setItem("webhook", url);
+        localStorage.setItem("name", name);
 
         // var newurl = localStorage.getItem("webhook");
         document.getElementById("webhook").value = localStorage.getItem("webhook");
+        document.getElementById("name").value = localStorage.getItem("name");
         // var webhookurl = localStorage.getItem("webhook");
 
         // chrome.storage.sync.set({ webhook: value}, function() {
@@ -28,6 +33,10 @@ function savewebhook() {
         chrome.storage.sync.set({
             webhook: url
         }, function () {});
+        chrome.storage.sync.set({
+            name1: name
+        }, function () {});
+
 
 
         document.getElementById("result").innerHTML = "should work";
