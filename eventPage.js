f73ae450b8a814f453abac2791947fc342726d71
Cornelius Denninger <cornelius.denninger@gmail.com>
2019-04-09 20:35:08 +0200

    asd

--- eventPage.js
+++ eventPage.js
@@ -1,14 +1,20 @@
-var webhook = "https://discordapp.com/api/webhooks/564579809661943838/fNuj_dNry8ZfgC_5Q8iLItpdnUHUnn44pJIzVy90MT3NcSjwhA9JWZMz6ZtG9V_-m3TL";
+// var webhook = "https://discordapp.com/api/webhooks/564579809661943838/fNuj_dNry8ZfgC_5Q8iLItpdnUHUnn44pJIzVy90MT3NcSjwhA9JWZMz6ZtG9V_-m3TL";
 var contextMenuItem = {
 	"id": "discord",
 	"title": "Send Link to Discord",
 	"contexts": ["all"],
 };
-
+var webhook = "";
 chrome.contextMenus.create(contextMenuItem);
-
+chrome.storage.sync.get('webhook', function (data) {
+	console.log(data.webhook);
+	webhook = data.webhook;
+});
 chrome.contextMenus.onClicked.addListener(function (pageUrl) {
-	
+	chrome.storage.sync.get('webhook', function (data) {
+		console.log(data.webhook);
+		webhook = data.webhook;
+	});
 	var xhttp = new XMLHttpRequest();
 	var URL = pageUrl.pageUrl;
 	console.log(URL);
@@ -18,3 +24,7 @@ chrome.contextMenus.onClicked.addListener(function (pageUrl) {
 		"content": URL
 	}));
 });
+
+
+// chrome.storage.sync.set({ key : value}, function() {
+// });
--- popout.js
+++ popout.js
@@ -14,11 +14,25 @@ function savewebhook() {
 
     if (typeof (Storage) !== "undefined") {
         // Store
-        localStorage.setItem("webhook", webhookurl);
+        var url = document.getElementById("webhook").value;
+        localStorage.setItem("webhook", url);
+
+        // var newurl = localStorage.getItem("webhook");
+        document.getElementById("webhook").value = localStorage.getItem("webhook");
+        // var webhookurl = localStorage.getItem("webhook");
+
+        // chrome.storage.sync.set({ webhook: value}, function() {
+        //     });
+
+
+        chrome.storage.sync.set({
+            webhook: url
+        }, function () {});
+
+
         document.getElementById("result").innerHTML = "should work";
-    
+
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
 }
-
